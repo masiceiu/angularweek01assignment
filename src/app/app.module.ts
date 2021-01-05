d9ee1432c1b5c19ab37fc6a44771711ed0d5246b
Abdus.Sobhan <masiceiu@gmail.com>
2021-01-05 15:07:15 +0600

    initial week 01 assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HtmlCssComponent } from './others/html-css/html-css.component';
+import { JavaScriptComponent } from './others/java-script/java-script.component';
+import { AngularComponent } from './others/angular/angular.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HtmlCssComponent,
+    JavaScriptComponent,
+    AngularComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
